89b8bd3859cdb9d1caef5318a03326a3980852ae
Brian Sanchez <briansanchezc17@gmail.com>
2021-12-22 13:03:39 -0300

    Update of Portfolio

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -2,3 +2,13 @@ window.addEventListener("scroll", () => {
     let nav = document.querySelector("nav");
     nav.classList.toggle("bajar", window.scrollY>0);
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
